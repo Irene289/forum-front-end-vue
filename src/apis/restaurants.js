f2bdc7f581882a0ba2a09edb58c1ed5272941c53
Irene289 <esmuybonita18@gmail.com>
2022-05-06 11:07:09 +0800

    Refactor API Helper

--- src/apis/restaurants.js
+++ src/apis/restaurants.js
@@ -1,35 +1,19 @@
 import { apiHelper } from "../utils/helpers"
-const getToken = () => localStorage.getItem('token')
 
 export default {
   getRestaurant({ restaurantId }) {
-    return apiHelper.get(`/restaurants/${restaurantId}`, {
-      headers: { Authorization: `Bearer ${getToken()}` }
-    })
+    return apiHelper.get(`/restaurants/${restaurantId}`)
   },
   getRestaurants ({ page, categoryId }) {
     // 帶入 queryString
     const searchParams = new URLSearchParams({page, categoryId})
     // 透過apiHelper發出GET請求給/restaurants
-    return apiHelper.get(`/restaurants?${searchParams.toString()}`, {
-      // 從 LocalStorage 取出 token，放進 Header
-      headers: { Authorization: `Bearer ${getToken()}` }
-    })
+    return apiHelper.get(`/restaurants?${searchParams.toString()}`)
   },
   getFeeds() {
-    return apiHelper.get('/restaurants/feeds', {
-      headers: { Authorization: `Bearer ${getToken()}` }
-    })
+    return apiHelper.get('/restaurants/feeds')
   },
   getTopRestaurants () {
-    return apiHelper.get('/restaurants/top', {
-      headers: { Authorization: `Bearer ${getToken()}` }
-    })
-  },
-  // 改用 getRestaurant
-  // getDashboard({ restaurantId }) {
-  //   return apiHelper.get(`/restaurants/${restaurantId}/dashboard`, {
-  //     headers: { Authorization: `Bearer ${getToken()}` }
-  //   })
-  // }
+    return apiHelper.get('/restaurants/top')
+  }
 }
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2,10 +2,28 @@ import axios from 'axios'
 import Swal from 'sweetalert2'
 
 const baseURL = 'http://localhost:3000/api'
-
-export const apiHelper = axios.create({
+const axiosInstance = axios.create({
   baseURL
 })
+
+// Add a request interceptor
+axiosInstance.interceptors.request.use(function (config) {
+  // Do something before request is sent
+  // 從 localStorage 將 token 取出
+  const token = localStorage.getItem('token')
+
+  // 如果 token 存在的話，則帶入到 headers 當中
+  if (token) {
+    config.headers['Authorization'] = `Bearer ${token}`
+  }
+
+  return config
+}, function (error) {
+  // Do something with request error
+  return Promise.reject(error);
+})
+
+export const apiHelper = axiosInstance
 
 export const Toast = Swal.mixin({
   toast: true,
